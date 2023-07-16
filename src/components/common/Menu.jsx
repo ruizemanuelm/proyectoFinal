a941bac57db2a5bd7f76fb7f3486d64cd18e13bf
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-07-16 16:04:54 -0300

    empezando el menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,45 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
+import logo from '../../assets/logo.png'
+
 
 const Menu = () => {
     return (
-        <div>
-            
-        </div>
+         <Navbar expand="lg" className="bg-white p-4"  >
+      <Container >
+        <Navbar.Brand href="#"><img src={logo} alt="logo"  width='150' height='90' /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            style= {{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
