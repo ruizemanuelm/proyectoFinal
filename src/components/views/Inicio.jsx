c384a1b845e436b9f5fc6bfdf01e07478b0b3768
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-17 20:17:02 -0300

    formato de las cards que van a ser utilizadas en el inicio

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -33,6 +33,51 @@ const Inicio = () => {
           />
         </Carousel.Item>
       </Carousel>
+      <Container>
+        <CardGroup className="mt-5 mb-5">
+          <Card>
+            <Card.Body className="justify-content-center">
+              <Card.Title className="text-danger text-center">
+                Atencion online
+              </Card.Title>
+              <Card.Text className="text-center">
+                Garantizamos la mejor atencion.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-danger text-center">
+                Atencion personalizada a domicilio
+              </Card.Title>
+              <Card.Text className="text-center">
+                Rapida atencion en casos de urgencia.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-danger text-center">
+                Salud preventiva
+              </Card.Title>
+              <Card.Text className="text-center">
+                La prevención es la clave para una vida larga y saludable.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-danger text-center">
+                Hospitalización cómoda
+              </Card.Title>
+              <Card.Text className="text-center">
+                Tu mascota puede quedarse en nuestras instalaciones.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        <hr className="mt-5" />
+      </Container>
         </section>
     );
 };
---
+++ src/components/views/publicidad/CarouselCards.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Card, Container } from 'react-bootstrap';
+
+const CarouselCards = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CarouselCards;
