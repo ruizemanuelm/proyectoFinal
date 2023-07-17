df26f88783adf2bce3984515564ac845cc1eae68
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-17 20:14:16 -0300

    carousel agreagado

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import {
+    Container,
+    Col,
+    Card,
+    CardGroup,
+    Carousel,
+    Button,
+  } from "react-bootstrap";
 
 const Inicio = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="mainSection">
+                  <Carousel slide={false}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={banner}
+            alt="Oferta de temporada"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={banner1}
+            alt="Servicios disponibles"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={banner2}
+            alt="Marcas con las que trabajamos"
+          />
+        </Carousel.Item>
+      </Carousel>
+        </section>
     );
 };
 
