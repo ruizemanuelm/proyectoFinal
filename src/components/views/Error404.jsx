cd5d61ecd558b1f8f0f776afa6ceebfa4bf9e017
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-17 20:41:29 -0300

    pagina de error 404 terminada

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,13 +1,13 @@
 import { Button } from 'react-bootstrap';
 import error from '../../assets/error404.gif'
-import { Link } from 'react-router-dom';
+
 
 const Error404 = () => {
     return (
         <section className="mainSection text-center">
             <img src={error} alt="error 404" />
             <div>
-            <Button as={Link} to={'/'} variant='dark' >Volver a casa</Button>
+            <Button variant='dark' >Volver a casa</Button>
 
             </div>
         </section>
