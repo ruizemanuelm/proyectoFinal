2845e52d1a1d060c32f2ec59563fb0c317b33534
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-19 22:35:49 -0300

    cambiar inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Menu from './components/common/Menu';
+
 import Inicio from './components/views/Inicio';
+
 
 function App() {
 
   return (
     <>
+
       <Inicio></Inicio>
+
     </>
   )
 }
