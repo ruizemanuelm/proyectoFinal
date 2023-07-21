f316d8118c8b350b9ef968056481e9fbed943f83
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-07-20 23:06:47 -0300

    creacion de panel admin general

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Menu from './components/common/Menu';
+import Admin from './components/views/Admin';
 
 function App() {
 
   return (
     <>
       <Menu></Menu>
+      <Admin></Admin>
     </>
   )
 }
--- src/components/views/Admin.jsx
+++ src/components/views/Admin.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import Tab from 'react-bootstrap/Tab';
+import {Tabs, Card, Table} from 'react-bootstrap';
 
 const Admin = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='container mainSection'>
+                <Tabs
+      defaultActiveKey="profile"
+      id="AdminGeneral"
+      className="mb-3"
+      fill
+      variant="underline"
+    >
+      <Tab eventKey="Inicio" title="Inicio">
+      <Card>
+      <Card.Header>Bienvenido</Card.Header>
+      <Card.Body>
+        <Card.Title>Turnos del dia</Card.Title>
+        <Card.Text>
+
+        </Card.Text>
+        <Table responsive striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Horario</th>
+            <th>Paciente</th>
+            <th>Dueño</th>
+            <th>Detalle de la consulta</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </Table>
+      </Card.Body>
+    </Card>
+          </Tab>
+      <Tab eventKey="Perfil" title="Perfil">
+        Tab content for Profile
+      </Tab>
+      <Tab eventKey="Pacientes" title="Pacientes">
+        Tab content for Loooonger Tab
+      </Tab>
+      <Tab eventKey="Turnos" title="Turnos" >
+        Tab content for Contact
+      </Tab>
+    </Tabs>
+        </section>
     );
 };
 
