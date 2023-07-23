7f250caf850c65d2fccfb2b9166de7b52834af39
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-07-22 23:09:41 -0300

    se agrego en el menu solicitar turno

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Menu from './components/common/Menu';
 import Inicio from './components/views/Inicio';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+      <Menu></Menu>
       <Inicio></Inicio>
+      
+      </BrowserRouter>
+      
     </>
   )
 }
