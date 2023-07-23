a466a39ce54372dd332afd83b7e77ecb76560f90
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-07-23 02:44:53 -0300

    agregamos las imagenes de nuestro equipo de trabajo y descripcion

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Menu from './components/common/Menu';
 import Admin from './components/views/Admin';
+import Nosotros from './components/views/Nosotros';
 
 
 
@@ -10,8 +11,8 @@ function App() {
 
   return (
     <>
-      <Menu></Menu>
-      <Admin></Admin>
+      <Nosotros></Nosotros>
+      
     </>
   )
 }
---
+++ src/components/views/Nosotros.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Button, Card,  CardGroup,  Container, Row } from 'react-bootstrap';
+import sebastian from '../../assets/sebastian.jpeg'
+import lucio from '../../assets/lucio.jpg'
+import emanuel from '../../assets/emanuel.jpg'
+import { NavLink, Link } from 'react-router-dom';
+
+
+const Nosotros = () => {
+
+      const miembros = [
+        {
+          nombre: 'Lucio Lazarte',
+          descripcion: 'Estudiante de Programación',
+          edad: 'Edad: 22',
+          imagen: lucio,
+        },
+        {
+          nombre: 'Matias Emanuel Ruiz Villalobo',
+          descripcion: 'Estudiante de Programación',
+          edad: 'Edad: 22',
+          imagen: emanuel,
+        },
+        {
+          nombre: 'Sebastián Orosco',
+          descripcion: 'Estudiante de Programación',
+          edad: 'Edad: 27',
+          imagen: sebastian,
+        },
+      ];
+    
+      return (
+
+        <section className="container mt-5">
+          <h1 className="display-4 text-center py-5">Acercá de nosotros</h1>
+          <h3 className='text-center'>En nuestra veterinaria, cuidamos con amor y experiencia a nuestros amigos peludos, brindando atención de calidad y servicios especializados para garantizar su bienestar en cada etapa de sus vidas. ¡Porque su salud y felicidad son nuestra pasión!</h3>
+          
+          <h2 className="display-4 text-center py-5">Equipo de Trabajo</h2>
+          <hr/>
+      <article className="row">
+        {miembros.map((miembro, index) => (
+          <aside className="col-md-12 card mb-5 border-0">
+              <div className="row m-5">
+                <div className="col-md-6">
+                  <img
+                    src={miembro.imagen}
+                    className="card-img-top img-fluid rounded-circle mx-auto d-block"
+                    alt={miembro.nombre}
+                    style={{ height: '200px', width: '200px', objectFit: 'cover' }}
+                  />
+                </div>
+                <div className="col-md-6 ">
+                  <div className="card-body">
+                    <h5 className="card-title ">{miembro.nombre}</h5>
+                    <p className="card-text ">{miembro.descripcion}</p>
+                    <p className="card-text ">{miembro.edad}</p>
+                  </div>
+                </div>
+              </div>
+            </aside>
+          
+        ))}
+      </article>
+    </section>
+     
+    
+      
+    );
+};
+
+export default Nosotros;
