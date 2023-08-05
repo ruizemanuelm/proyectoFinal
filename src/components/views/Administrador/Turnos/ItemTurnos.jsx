ca22ef2095bc12772e5ca3530f57b73b08b28be5
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-08-05 16:42:21 -0300

    sweet alert para confimacion al borrar turno

--- src/components/views/Administrador/Turnos/ItemTurnos.jsx
+++ src/components/views/Administrador/Turnos/ItemTurnos.jsx
@@ -6,27 +6,53 @@ import { borrarTurnos } from '../../../helpers/queries';
 import Swal from 'sweetalert2';
 
 const ItemTurnos = ({turnos,setTurnos}) => {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success m-1',
+      cancelButton: 'btn btn-danger m-1'
+    },
+    buttonsStyling: false
+  })
+
   const borrarTurno = () => {
-    borrarTurnos(turnos._id).then((respuesta) => {
-      if (respuesta && respuesta.status === 200) {
-        Swal.fire(
-          "Turno eliminado",
-          `El Turno fue eliminado correctamente`,
-          "success"
-        );
-        //recargar la tabla
-        obtenerTurnos().then((respuesta) => {
-          if (respuesta) {
-            setTurnos(respuesta);
-          }
-        });
-      } else {
-        Swal.fire(
-          "Ocurrió un error",
-          `El Turno no pudo ser eliminado`,
-          "error"
-        );
-      }
+
+
+swalWithBootstrapButtons.fire({
+  title: '¿Seguro que desea eliminar el turno?',
+  text: "una vez realizada esta operación, no podrá deshacer los cambios efectuados",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonText: 'Borrar',
+  cancelButtonText: 'Cancelar',
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+
+    borrarTurnos(turnos._id).then((respuesta) => { 
+    if (respuesta && respuesta.status === 200) {
+      swalWithBootstrapButtons.fire(
+        'Eliminado!',
+        'El turno ha sido eliminado correctamente',
+        'success'
+      )
+      obtenerTurnos().then((respuesta) => {
+        if (respuesta) {
+          setTurnos(respuesta);
+        }
+      });
+    } else {
+      swalWithBootstrapButtons.fire(
+        'Ocurrió un error',
+        'El Turno no pudo ser eliminado',
+        'error')} 
+  })
+    } else if (
+    result.dismiss === Swal.DismissReason.cancel) {
+    swalWithBootstrapButtons.fire(
+      'Cancelado',
+      'Los cambios realizados no se guardaron',
+      'error'
+    )}
     });
   };
 
