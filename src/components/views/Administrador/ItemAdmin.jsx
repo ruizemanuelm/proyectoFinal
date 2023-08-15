fc566b5d2989553e6a786aa707918b2c3c0d4b34
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-08-15 02:51:57 -0300

    agregue imagen de veterinario en admin-inicio

--- src/components/views/Administrador/ItemAdmin.jsx
+++ src/components/views/Administrador/ItemAdmin.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
+import { CardImage } from "react-bootstrap-icons";
+import CardHeader from "react-bootstrap/esm/CardHeader";
 
 const ItemAdmin = ({ turnos }) => {
+const vet1Img = "https://static.vecteezy.com/system/resources/previews/014/175/730/non_2x/young-man-with-blue-shirt-free-vector.jpg"
+const vet2Img = "https://static.vecteezy.com/system/resources/previews/014/175/097/non_2x/beauty-young-woman-free-vector.jpg"
+const imagenURL = turnos.veterinario === 'Juan Carlos' ? vet1Img : vet2Img;
   return (
     <>
       <Col sm={12} md={4}>
         <Card className="m-1 h-100">
+            <CardHeader className="fs-4 bg-white d-flex justify-content-around align-items-center">
+              <img className="w-25" src={imagenURL} ></img>
+              <b>{turnos.veterinario} </b> </CardHeader>
           <Card.Body>
             <Card.Title>
               <b className="text-danger">Mascota: </b> {turnos.nombMascota}
@@ -17,9 +25,14 @@ const ItemAdmin = ({ turnos }) => {
               {turnos.detalleCita}
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-">
-              {turnos.hora + " - " + turnos.fechaTurno}
+          <Card.Footer className="d-flex justify-content-between">
+            <small className="fs-5">
+            {turnos.fechaTurno}
+             </small>
+            <small className="text-danger fs-5">
+              <b>
+              {turnos.hora}Hs.
+              </b>
             </small>
           </Card.Footer>
         </Card>
