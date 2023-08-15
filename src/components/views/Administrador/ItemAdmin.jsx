fcedf293a6882190cc06b3c68379525298431a16
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-08-15 01:52:28 -0300

    ordene los turnos por fecha y hora

--- src/components/views/Administrador/ItemAdmin.jsx
+++ src/components/views/Administrador/ItemAdmin.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Button, Card,Col } from 'react-bootstrap';
+import React from "react";
+import { Card, Col } from "react-bootstrap";
 
-const ItemAdmin = ({turnos}) => {    
-    return (
-        <>
-        <Col sm={12} md={4}>
-         <Card className='m-1 h-100'>
-        <Card.Body>
-          <Card.Title><b className='text-danger'>Mascota:  </b>  {turnos.nombMascota}</Card.Title>
-          <hr />
-          <Card.Text> <b className='text-danger'>Detalle de la cita: </b> 
-           {turnos.detalleCita}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-">{turnos.hora + " - " + turnos.fechaTurno}</small>
-        </Card.Footer>
-      </Card>
-        </Col>
-      </>
-    );
+const ItemAdmin = ({ turnos }) => {
+  return (
+    <>
+      <Col sm={12} md={4}>
+        <Card className="m-1 h-100">
+          <Card.Body>
+            <Card.Title>
+              <b className="text-danger">Mascota: </b> {turnos.nombMascota}
+            </Card.Title>
+            <hr />
+            <Card.Text>
+              {" "}
+              <b className="text-danger">Detalle de la cita: </b>
+              {turnos.detalleCita}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-">
+              {turnos.hora + " - " + turnos.fechaTurno}
+            </small>
+          </Card.Footer>
+        </Card>
+      </Col>
+    </>
+  );
 };
 
 export default ItemAdmin;
